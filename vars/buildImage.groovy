3cbbb6babc4a88f7fe74ea470e5167354017f074
vladibo13 <vladibo13@gmail.com>
2024-09-10 22:05:23 +0300

    added parameter to build image and jar

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,9 +1,10 @@
 #!/user/bin/env groovy
-def call() {
+def call(String imageName) {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t vladibo/demo-app:jma-1.2 .'
+        sh "docker build -t  $imageName ."
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push vladibo/demo-app:jma-1.2'
+        sh "docker push $imageName"
+        // vladibo/demo-app:jma-1.2
     }
 }
--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,4 +1,6 @@
+#!/user/bin/env groovy
+
 def call() {
-    echo 'building the application...'
+    echo "building the application for branch $BRANCH_NAME"
     sh 'mvn package'
 }
