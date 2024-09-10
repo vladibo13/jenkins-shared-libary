733f19c5f4134de09a1dd7674f17fc7f792397f3
vladibo13 <vladibo13@gmail.com>
2024-09-10 22:27:41 +0300

    testing for groovy syntax

---
+++ src/com.example/Docker.groovy
@@ -0,0 +1,21 @@
+#!/user/bin/env groovy
+
+package com.example
+
+class Docker implements Serializable{
+    def script
+
+    Docker(script) {
+        this.script = script
+    }
+
+    def buildDockerImage(String imageName) {
+        script.echo "building the docker image..."
+        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        script.sh "docker build -t  $imageName ."
+        script.sh "echo '${script.PASS}' | docker login -u '${script.USER}' --password-stdin"
+        script.sh "docker push $imageName"
+        // vladibo/demo-app:jma-1.2
+        }
+    }
+}
--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,6 @@
 #!/user/bin/env groovy
+import com.example.Docker
+
 def call(String imageName) {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker build -t  $imageName ."
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh "docker push $imageName"
-        // vladibo/demo-app:jma-1.2
-    }
+    return new Docker(this).buildDockerImage(imageName)
 }
